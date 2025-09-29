8530c2d1fb4884d2ec8a8d3747bc29af1895869f
Delvenne Gauthier <gauthier.delvenne@student.hepl.be>
2025-09-29 15:48:00 +0200

    restructe constants

--- code/tictic/lib/constants/colors.dart
+++ code/tictic/lib/constants/colors.dart
@@ -15,7 +15,7 @@ final kShadow = BoxShadow(
   offset: const Offset(0, 3), // changes position of shadow
 );
 
-final appTheme = ThemeData(
+final kAppTheme = ThemeData(
   useMaterial3: true,
   // recommandé avec ColorScheme
   colorScheme: ColorScheme(
